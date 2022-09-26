952b5d95fba75c3bb12c9e958f64165d4fa881f9
Bislon Zulu <bislonzulu1@outlook.com>
2022-09-26 22:14:55 +0200

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,5 @@
 #!usr/bin/python3
 
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i == "c" or i == "C":
-            continue
-        else:
-            new_string += i
-    return new_string
+    output - my_string.translate({ord(i): None for i in 'cC'})
+    return output
