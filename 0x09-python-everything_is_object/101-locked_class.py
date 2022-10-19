1b8fe92e95ebc200df608738b4eee4fa629e8abb
Bislon Zulu <bislonzulu1@outlook.com>
2022-10-19 22:36:39 +0200

    tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+    Definition for class LockedClass.
+"""
+
+class LockedClass:
+    """LockedClass that defines 1 attribute"""
+
+    __slots__ = ['first_name']
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,1 @@
+Read me for everything is an object in python project
