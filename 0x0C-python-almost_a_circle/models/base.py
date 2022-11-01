0c8dfb526e04f4e93f62e468b33cece5f2a88491
Bislon Zulu <bislonzulu1@outlook.com>
2022-11-01 19:48:29 +0200

    correction for task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,5 +11,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nd_objexts += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
