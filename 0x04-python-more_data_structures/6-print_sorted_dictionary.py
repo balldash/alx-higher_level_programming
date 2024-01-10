208adffae97072a1965af38f3a59b7105d9c663b
Bislon Zulu <bislonzulu1@outlook.com>
2024-01-10 20:41:22 +0200

    task 7

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_dictionary = dict(sorted(a_dictionary.items()))
+    for key, value in sorted_dictionary.items():
+        print("{0}: {1}".format(key, value))
