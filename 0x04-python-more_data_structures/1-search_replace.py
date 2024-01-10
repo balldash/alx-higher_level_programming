74373fb50ddee475cf46fb3f3e09419b4cb87b83
Bislon Zulu <bislonzulu1@outlook.com>
2024-01-10 19:57:22 +0200

    task 2

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in range(len(my_list)):
+        if search == my_list[i]:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i])
+    return new_list
