70d491ba132b20b6259df27a684aa654729aa294
Bislon Zulu <bislonzulu1@outlook.com>
2024-07-09 23:38:47 +0200

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,6 +5,6 @@ takes in a URL, sends a request to the URL and displays the value of X-request-I
 import urllib.request
 from sys import argv
 
-with urllib.request.urlopen(arg[1] as res
+with urllib.request.urlopen(argv[1]) as res:
         htmlId = res.info().get('X-Request-Id')
         print(htmlId)
